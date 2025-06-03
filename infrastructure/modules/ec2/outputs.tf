88f94c083fdd648e2682b82f15ea546fce248d85
Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>
2025-06-03 15:52:12 +0200

    Outputting key and DNS

--- infrastructure/modules/ec2/outputs.tf
+++ infrastructure/modules/ec2/outputs.tf
@@ -8,6 +8,11 @@ output "elastic_ip" {
   description = "Elastic IP address of the first EC2 instance (if any)"
 }
 
+output "public_dns" {
+  value = try(aws_instance.this[0].public_dns, null)
+  description = "Public DNS of the first EC2 instance (if any)"
+}
+
 output "ec2_private_key_pem" {
   value       = tls_private_key.ec2.private_key_pem
   sensitive   = true
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -7,12 +7,24 @@ output "public_subnet_id" {
 }
 
 output "ec2_public_ip" {
-  value = module.ec2.public_ip
+  value       = module.ec2.public_ip
+  description = "Public IPv4 address of the first EC2 instance"
 }
 
 output "ec2_elastic_ip" {
   value = module.ec2.elastic_ip
   description = "Elastic IP address of the first EC2 instance (if any)"
+}
+
+output "ec2_private_key_pem" {
+  value       = module.ec2.ec2_private_key_pem
+  sensitive   = true
+  description = "PEM-encoded private key for EC2 SSH access"
+}
+
+output "ec2_public_dns" {
+  value       = module.ec2.public_dns
+  description = "Public DNS of the first EC2 instance"
 }
 
 output "rds_endpoint" {

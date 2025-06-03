63578c47eedefd863d5fcd83cd9894bf3ce0998b
Daniel van Zyl <daniel.vanzyl@bbd.co.za>
2025-06-03 14:15:40 +0200

    Initialised node and express

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/static", express.static(path.join(__dirname, "../public/static")));
+
+app.get("/{*any}", (_request, response) => {
+  response.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default app;

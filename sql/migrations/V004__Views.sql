9a3ab756602070fad486ee2042bcae430762bb86
Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>
2025-06-04 15:18:44 +0200

    Renamed

---
+++ sql/migrations/V004__Views.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE VIEW all_teams AS
+SELECT
+    t.team_id,
+    t.team_name,
+    t.task_description
+    COUNT(m.member_id) AS member_count
+FROM teams t
+INNER JOIN members m ON t.team_id = m.team_id
+WHERE m.removed_at IS NULL
+GROUP BY t.team_id
+ORDER BY t.team_name ASC;

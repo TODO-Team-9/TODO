1a9cb779bd63e26857d808790278017e38cbb311
Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>
2025-06-09 14:22:16 +0200

    Cleaning up

--- sql/migrations/V014__Update_Create_Task_Proc.sql
+++ sql/migrations/V014__Update_Create_Task_Proc.sql
@@ -8,18 +8,15 @@ CREATE OR REPLACE PROCEDURE create_task(
 DECLARE
     v_status_id INT;
 BEGIN
-    -- Validate priority exists
     IF NOT EXISTS (SELECT 1 FROM priorities WHERE priority_id = p_priority_id) THEN
         RAISE EXCEPTION 'Priority does not exist';
     END IF;
 
-    -- Get default status (Backlog)
     SELECT status_id INTO v_status_id FROM statuses WHERE status_name = 'Backlog';
     IF v_status_id IS NULL THEN
         RAISE EXCEPTION 'Default status "Backlog" does not exist';
     END IF;
 
-    -- Insert the new task
     INSERT INTO tasks (
         task_name, task_description, team_id, status_id, priority_id, member_id
     ) VALUES (
